5484de653233598d8dec493e848083a6a791f88f
lucyfayesmith <41600878+lucyfayesmith@users.noreply.github.com>
2021-05-26 13:23:08 +1000

    Final commit of our theme
    this commit aims to enable deletion of our previous theme folder that contained unncessary files

---
+++ cp3402-2021-team14-theme/single.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package CP3402_2021_Team14_Theme
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main site-main-program">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', get_post_type() );
+
+			the_post_navigation(
+				array(
+					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous post:', 'cp3402-2021-team14-theme' ) . '</span> <span class="nav-title">%title</span>',
+					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next post:', 'cp3402-2021-team14-theme' ) . '</span> <span class="nav-title">%title</span>',
+				)
+			);
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+
+	</main><!-- #main -->
+
+<?php
+get_footer();
