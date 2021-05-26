af8cc7388021c00358bc9b341b07e11854419008
Lucy <lucy.smith2@my.jcu.edu.au>
2021-05-26 10:39:12 +1000

    update navigation bar, add footer details and adjust header

--- wp-content/themes/cp3402-2021-team14-theme/footer.php
+++ wp-content/themes/cp3402-2021-team14-theme/footer.php
@@ -13,17 +13,23 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'cp3402-2021-team14-theme' ) ); ?>">
+			<a href="<?php echo esc_url( __( 'http://54.253.220.234/', 'cp3402-2021-team14-theme' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'cp3402-2021-team14-theme' ), 'WordPress' );
+				printf( esc_html__( 'Townsville Jazz Club' ));
 				?>
 			</a>
 			<span class="sep"> | </span>
 				<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'cp3402-2021-team14-theme' ), 'cp3402-2021-team14-theme', '<a href="http://underscores.me/">Underscores.me</a>' );
+				printf( esc_html__( 'Address: 124 Wellington St, Aitkenvale QLD 4814' ));
 				?>
+
+            <span class="sep"> | </span>
+            <?php
+            /* translators: 1: Theme name, 2: Theme author. */
+            printf( esc_html__( 'Mobile: 0431 618 618' ));
+            ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
